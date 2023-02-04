ab9e04c1f4ca5ecbd8989f03238a25a70f52c5a8
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2023-02-04 15:26:16 -0500

    update createWorkout route w/ isLoggedIn fxn

--- routes/createWorkout.js
+++ routes/createWorkout.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const workoutCtrl = require('../controllers/workouts');
 const isLoggedIn = require('../config/auth');
 
-router.get('/', workoutCtrl.index);
-router.get('/new', workoutCtrl.new);
-router.get('/:id', workoutCtrl.show);
-router.post('/', workoutCtrl.create);
+router.get('/', isLoggedIn, workoutCtrl.index);
+router.get('/new', isLoggedIn, workoutCtrl.new);
+router.get('/:id', isLoggedIn, workoutCtrl.show);
+router.post('/', isLoggedIn, workoutCtrl.create);
 
 module.exports = router;
