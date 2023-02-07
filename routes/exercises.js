0255aa798843c7462c5bc4ee5fdf2b78ca8a923e
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2023-02-07 17:18:06 -0500

    update

--- routes/exercises.js
+++ routes/exercises.js
@@ -8,7 +8,7 @@ router.get('/', isLoggedIn, exercisesCtrl.index);
 router.get('/new', isLoggedIn, exercisesCtrl.new);
 router.get('/:id', isLoggedIn, exercisesCtrl.show);
 router.post('/', isLoggedIn, exercisesCtrl.create);
-router.delete('/exercises/:id', exercisesCtrl.delete);
+router.get('/exercises/:id', exercisesCtrl.delete);
 router.get('/:id/edit', isLoggedIn, exercisesCtrl.edit);
 router.put('/:id', isLoggedIn, exercisesCtrl.update);
 
